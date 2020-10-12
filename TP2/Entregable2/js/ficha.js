afdb6b4f3b156d5e106b4799d2baf09cfec2c249
Leonel Dimasi <leoneldimasi@gmail.com>
2020-10-12 15:31:42 -0300

    replanteo de ficha

---
+++ TP2/Entregable2/js/ficha.js
@@ -0,0 +1,67 @@
+export class Ficha {
+    x;
+    y;
+    radio;
+    color;
+    ctx;
+    habilitada ;
+    seleccionada ;
+    constructor(x, y, radio, color, ctx) {
+        this.x = x;
+        this.y = y;
+        this.radio = radio;
+        this.color = color;
+        this.ctx = ctx;
+        this.habilitada = true;
+        this.seleccionada = false;
+    }
+    
+    dibujar() {
+        this.ctx.beginPath();
+        this.ctx.fillStyle = this.color;
+        this.ctx.arc(this.x,this.y,this.radio, 0, Math.PI * 2);
+        this.ctx.fill();
+        this.ctx.closePath();
+    }
+
+    mover(x,y,tablero,ficha2) {
+        if (this.isHabilitada()){
+        this.ctx.clearRect(0, 0, canvas.width,canvas.height);
+        tablero.dibujar();
+        this.x = x;
+        this.y = y;
+        ficha2.dibujar();
+        this.dibujar();
+        }
+    }
+    isSeleccionada(){
+        return this.seleccionada;
+    }
+
+    seleccionar(){
+        this.seleccionada = true;
+    }
+
+    deseleccionar() {
+        this.seleccionada = false;
+    }
+
+    isClick(xM,yM) {
+        let x = xM - this.x;
+        let y = yM - this.y;
+        return (Math.sqrt(x*x + y*y) < (this.radio*2));
+    }
+
+    isHabilitada() {
+        return this.habilitada;
+    }
+
+    habilitar() {
+        this.habilitada = true;
+    }
+
+    deshabilitar() {
+        this.habilitada = false;
+    }
+}
+      
